940c8a6747b1164eb102f48d036040e1146b3659
Deepak <DVasoya2035@conestogac.on.ca>
2024-10-02 08:52:45 -0400

    Add ing todos and deleting todos.

---
+++ src/app/myComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from '../../Todo';
+
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+
+  @Input() todo: Todo;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+
+  onclick(todo: Todo) {
+    console.log("On Clcik has been trigerred.")
+    this.todoDelete.emit(todo);
+  }
+  
+
+}
--- src/app/myComponents/todos/todos.component.ts
+++ src/app/myComponents/todos/todos.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { Todo } from '../../Todo';
+import { CommonModule } from '@angular/common';
+import { TodoItemComponent } from "../todo-item/todo-item.component";
+import { AddTodoComponent } from "../add-todo/add-todo.component";
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TodoItemComponent, AddTodoComponent, FormsModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
+    todos: Todo[];
+    constructor(){
+      this.todos = [
+        {
+          sno:1,
+          title: "This is title1",
+          description: "desc for todo",
+          active: true
+        },
+        {
+          sno:2,
+          title: "This is title2",
+          description: "desc for todo",
+          active: true
+        },
+        {
+          sno:3,
+          title: "This is title3",
+          description: "desc for todo",
+          active: true
+        }
+      ]
+    }
 
+
+    deleteTodo(todo: Todo){
+      console.log(todo);
+      const index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    }
+
+    addTodo(todo: Todo){
+      console.log(todo);
+      this.todos.push(todo);
+    }
+    
 }
