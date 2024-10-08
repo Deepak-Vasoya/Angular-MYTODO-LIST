32feab571cbab77b08c80eca048a256a70a9832a
Deepak <DVasoya2035@conestogac.on.ca>
2024-10-08 08:13:56 -0400

    Added routing concepts and About page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import {TodosComponent } from './myComponents/todos/todos.component';
 import {TodoItemComponent } from './myComponents/todo-item/todo-item.component'
 import { AddTodoComponent } from './myComponents/add-todo/add-todo.component';
+import { AboutComponent } from './myComponents/about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent, TodosComponent, TodoItemComponent, AddTodoComponent
+    AppComponent, TodosComponent, TodoItemComponent, AddTodoComponent, AboutComponent
   ],
   imports: [
     BrowserModule,
